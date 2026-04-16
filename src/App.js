08f5325ad102eba9c2050dbac8b12b5de7c5e6e8
Mel Tallagsen <mel@a2gdesigns.com>
2026-04-16 04:29:17 -0400

    Deploy: redesign https://outsmartpest.com using an up to d (Fork)

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>
+        A &amp; <strong>some B</strong>
+      </h1>
+    </main>
+  );
+}
